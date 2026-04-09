fc141dc3f91627239c3f1cf953dba5fcb14aa7da
neikuy <neikuylin@gmail.com>
2026-04-09 22:49:19 +0800

    脚本与绑定

---
+++ Assets/Scripts/场景绑定/主视角对话绑定.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public sealed class 主视角对话绑定 : MonoBehaviour
+{
+    [Header("预制体")]
+    public GameObject 对话预制体;
+
+    [Header("绑定")]
+    public GameObject 立绘容器;
+    public GameObject 角色名字;
+    public GameObject 对话内容;
+}
